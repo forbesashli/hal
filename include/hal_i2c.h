3130046b1920423f352654112519eb39cd608c11
Ashli <forbesashli@gmail.com>
2025-07-20 16:01:10 -0700

    i2c wrapper add

--- include/hal_i2c.h
+++ include/hal_i2c.h
@@ -6,19 +6,29 @@
  * Header file for I2C HAL abstraction, lightweight version for what I have been using for beargotchi.
  */
 
-
 #ifndef HAL_I2C_H
 #define HAL_I2C_H
 
 #include "hal.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
+// should change to a component specific header file instead
+#include "stm32h7xx_hal.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-    hal_error_E i2c_init(I2C_TypeDef *I2C);
-    hal_error_E i2c_send_addr(I2C_TypeDef *I2C, uint8_t addr);
-    hal_error_E i2c_send_data(I2C_TypeDef *I2C, uint8_t data);
-    hal_error_E i2c_read_data(I2C_TypeDef *I2C);
+
+typedef struct {
+    I2C_TypeDef *I2C;  // Pointer to the I2C peripheral
+    I2C_HandleTypeDef *hi2c; // Handle for the I2C peripheral
+    uint32_t Timing;   // Timing configuration for the I2C
+} hal_i2c_handle_S;
+
+hal_error_E i2c_init(hal_i2c_handle_S *I2C_Handle,  I2C_HandleTypeDef *hi2c, I2C_TypeDef *I2C, uint32_t Timing);
+hal_error_E i2c_master_send_data(hal_i2c_handle_S *I2C_Handle, uint8_t data);
+hal_error_E i2c_read_data(hal_i2c_handle_S *I2C_Handle);
 
 #ifdef __cplusplus
 }
--- src/hal_i2c.c
+++ src/hal_i2c.c
@@ -1,7 +1,31 @@
 #include "hal_i2c.h"
 
 
-hal_error_E i2c_init(I2C_TypeDef *I2C);
-hal_error_E i2c_send_addr(I2C_TypeDef *I2C, uint8_t addr);
-hal_error_E i2c_send_data(I2C_TypeDef *I2C, uint8_t data);
-hal_error_E i2c_read_data(I2C_TypeDef *I2C);
+hal_error_E i2c_init(hal_i2c_handle_S *I2C_Handle, I2C_HandleTypeDef *hi2c, I2C_TypeDef *I2C, uint32_t Timing) {
+    I2C_Handle->I2C = I2C;
+    I2C_Handle->hi2c = hi2c;
+    I2C_Handle->Timing = 0x60404E72;
+  
+    if (HAL_I2C_Init(I2C_Handle->hi2c) != HAL_ERROR_OK) {
+        return HAL_ERROR_ERR;
+    }
+    return HAL_ERROR_OK;
+}
+
+
+hal_error_E i2c_master_send_data(hal_i2c_handle_S *I2C_Handle, uint8_t data) {
+    // You may want to add a register address as argument
+    if (HAL_I2C_Master_Transmit(I2C_Handle->hi2c, I2C_Handle->hi2c->Init.OwnAddress1, &data, 1, 100) != HAL_ERROR_OK) {
+        return HAL_ERROR_ERR;
+    }
+    return HAL_ERROR_OK;
+}
+
+hal_error_E i2c_read_data(hal_i2c_handle_S *I2C_Handle) {
+    uint8_t data;
+    if (HAL_I2C_Master_Receive(I2C_Handle->hi2c, I2C_Handle->hi2c->Init.OwnAddress1, &data, 1, 100) != HAL_ERROR_OK) {
+        return HAL_ERROR_ERR;
+    }
+    // You may want to return the data or store it in the handle
+    return HAL_ERROR_OK;
+}
